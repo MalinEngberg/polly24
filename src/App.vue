751df41cf972ea1bd1c849fe99c5ca3943964886
camilaminotta <camecumi@outlook.com>
2025-12-10 11:26:45 +0100

    Ändrat färg och typsnitt

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,12 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style>
-  
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
+
+  html, body, #app {
+    height: 100%;
+    margin: 0;
+    background-color: #AA87BF;
+    font-family: 'Caveat', cursive;
+  }
 </style>
